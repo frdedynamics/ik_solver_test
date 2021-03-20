70b386e36f07881eca31a18a6ee3ba8fd29f6413
gizem <gizemates@iyte.edu.tr>
2021-03-20 23:43:11 +0100

    weight approach doesn't work. arm still moves. try to lock

--- robots/ur5/src/selective_ik_solver.py
+++ robots/ur5/src/selective_ik_solver.py
@@ -53,6 +53,8 @@ with env:
 # print "Tee-1:", manip.GetEndEffectorTransform() # get end effector
 # robot.SetDOFValues([0.0,0.0,1.57,0.0,0.0,0.0])
 # print "Tee-2:", manip.GetEndEffectorTransform() # get end effector
+# robot.SetDOFValues([0.0,0.0,1.57,pi/2,-pi/2,0.0])
+# print "Tee-3:", manip.GetEndEffectorTransform() # get end effector
 
 # Tee-1: [[-4.89630558e-12  1.00000000e+00  9.79261117e-12  1.18430000e+00]
  # [ 1.00000000e+00  4.89630558e-12  0.00000000e+00 -7.43859000e-01]
@@ -61,6 +63,10 @@ with env:
 # Tee-2: [[-3.83026943e-15  7.96326721e-04 -9.99999683e-01  4.96755774e-01]
  # [ 1.00000000e+00  4.89647212e-12  1.11022302e-16 -7.43859000e-01]
  # [ 4.89641661e-12 -9.99999683e-01 -7.96326721e-04  5.54908046e-01]
+ # [ 0.00000000e+00  0.00000000e+00  0.00000000e+00  1.00000000e+00]]
+ # Tee-3: [[ 9.99999683e-01  4.89652646e-12 -7.96326721e-04  7.04563574e-01]
+ # [-4.89652802e-12  1.00000000e+00 -1.33298292e-17 -8.36059000e-01]
+ # [ 7.96326721e-04  3.91256593e-15  9.99999683e-01  6.70773566e-01]
  # [ 0.00000000e+00  0.00000000e+00  0.00000000e+00  1.00000000e+00]]
 
 
@@ -72,18 +78,26 @@ basemanip = interfaces.BaseManipulation(robot)
 taskmanip = interfaces.TaskManipulation(robot)
 # robot.SetJointValues([-0.97],ikmodel2.manip.GetGripperIndices())
 robot.SetDOFValues([0.0,0.0,0.0,0.0,0.0,0.0])
-
+raw_input("cont?")
 # Tstart = np.array([[0.0,  0.0007, -0.999999683,  0.496755774], [ 1.00000000,  0.0,  0.0, -0.743859000],[ 0.0, -0.999999683, 0.0,  0.554908046], [ 0.00000000,  0.00000000,  0.00000000,  1.00000000]])
 Tee1 = np.array([[0.00,  1.00,  0.00,  1.18], [1.00,  0.00,  0.00, -0.743], [-0.00,  0.00, -1.00,  1.011], [0.00,  0.00,  0.00,  1.00]])
 Tee2 = np.array([[0.00,  0.00,  -1.00,  0.496], [ 1.00,  0.00,  0.00, -0.743], [0.00,  -1.00, 0.00,  0.555], [ 0.00,  0.00,  0.00,  1.00]])
-ikparam = IkParameterization(Tee1[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
-# iksolverbase = InterfaceBase.IkSolverBase.Init(robot) I don't know how to use IkSolverBase
+Tee3 = np.array([[1.00,  0.00, 0.00,  0.704], [0.00,  1.00, 0.00, -0.836], [0.00,  0.00,  1.00,  0.670], [0.00,  0.00,  0.00,  1.00]])
+ikparam1 = IkParameterization(Tee1[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
+ikparam2 = IkParameterization(Tee2[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
+ikparam3 = IkParameterization(Tee3[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
 handle = manip.GetIkSolver()
-sol = manip.FindIKSolution(ikparam,IkFilterOptions.CheckEnvCollisions)
+sol1 = manip.FindIKSolution(ikparam1,IkFilterOptions.CheckEnvCollisions)
+sol2 = manip.FindIKSolution(ikparam2,IkFilterOptions.CheckEnvCollisions)
+sol3 = manip.FindIKSolution(ikparam3,IkFilterOptions.CheckEnvCollisions)
 # print "Ik solver:", success # Ik solver: [ 5.55111512e-16 -3.96993549e-12  1.57000000e+00  1.11022302e-16  0.00000000e+00  0.00000000e+00] with manip.GetIkParameterization(IkParameterization.Type.Translation3D)
-print "Ik solver:", sol # Ik solver: [ [-1.11022302e-16  8.99614938e-10  1.57000000e+00  1.11022302e-16  0.00000000e+00  0.00000000e+00]] with ikparam
-robot.SetDOFValues(sol,ikmodel2.manip.GetArmIndices())
-# basemanip.MoveToHandPosition([Tstart],maxiter=1000,maxtries=1,seedik=4)
+print "Ik solver:", sol1 # Ik solver: [ [-1.11022302e-16  8.99614938e-10  1.57000000e+00  1.11022302e-16  0.00000000e+00  0.00000000e+00]] with ikparam
+robot.SetDOFValues(sol1,ikmodel2.manip.GetArmIndices())
+raw_input("cont2?")
+robot.SetDOFValues(sol2,ikmodel2.manip.GetArmIndices())
+raw_input("cont3?")
+robot.SetDOFValues(sol3,ikmodel2.manip.GetArmIndices())
+# basemanip.MoveToHandPosition([Tee2[0:3,3]],maxiter=1000,maxtries=1,seedik=4)
 # robot.WaitForController(0)
 raw_input("done")
 sys.exit()
