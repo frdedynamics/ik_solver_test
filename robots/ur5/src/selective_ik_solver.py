3b44b89eb6a29189c2810d20e38793b58e507c89
gizem <gizemates@iyte.edu.tr>
2021-03-21 00:07:54 +0100

    Joint limits work but no solution found. Adjusting the limits

--- robots/ur5/src/selective_ik_solver.py
+++ robots/ur5/src/selective_ik_solver.py
@@ -44,7 +44,7 @@ with env:
   print "IKname:", ikmodel2.getikname()
   freeparam = manip.GetIkSolver().GetFreeParameters() # from: http://openrave.org/docs/latest_stable/coreapihtml/classOpenRAVE_1_1IkSolverBase.html
   print "IK free param:", freeparam
-  weights = np.array([10.0, 10.0, 10.0, 0.1, 0.1, 0.1])
+  weights = np.array([1000.0, 1000.0, 1000.0, 0.1, 0.1, 0.1])
   # weights = np.ones(robot.GetDOF())
   robot.SetDOFWeights(weights)
   print "DOF weights:", robot.GetDOFWeights()
@@ -70,6 +70,12 @@ with env:
  # [ 0.00000000e+00  0.00000000e+00  0.00000000e+00  1.00000000e+00]]
 
 
+lower = np.concatenate((np.array([0.0, 0.0, 1.57]), np.array([1., 1., 1.])*-3.14159265))
+upper = np.concatenate((np.array([0.0, 0.0, 1.57]), np.array([1., 1., 1.])*3.14159265))
+# lower = np.ones(robot.GetDOF())*-3.14159265
+# upper = np.ones(robot.GetDOF())*3.14159265
+robot.SetDOFLimits(lower, upper)
+print "DOF limits:", robot.GetDOFLimits()
 
   
 if not ikmodel2.load():
@@ -77,38 +83,32 @@ if not ikmodel2.load():
 basemanip = interfaces.BaseManipulation(robot)
 taskmanip = interfaces.TaskManipulation(robot)
 # robot.SetJointValues([-0.97],ikmodel2.manip.GetGripperIndices())
-robot.SetDOFValues([0.0,0.0,0.0,0.0,0.0,0.0])
+robot.SetDOFValues([0.0,0.0,1.57,0.0,0.0,0.0])
 raw_input("cont?")
 # Tstart = np.array([[0.0,  0.0007, -0.999999683,  0.496755774], [ 1.00000000,  0.0,  0.0, -0.743859000],[ 0.0, -0.999999683, 0.0,  0.554908046], [ 0.00000000,  0.00000000,  0.00000000,  1.00000000]])
 Tee1 = np.array([[0.00,  1.00,  0.00,  1.18], [1.00,  0.00,  0.00, -0.743], [-0.00,  0.00, -1.00,  1.011], [0.00,  0.00,  0.00,  1.00]])
 Tee2 = np.array([[0.00,  0.00,  -1.00,  0.496], [ 1.00,  0.00,  0.00, -0.743], [0.00,  -1.00, 0.00,  0.555], [ 0.00,  0.00,  0.00,  1.00]])
 Tee3 = np.array([[1.00,  0.00, 0.00,  0.704], [0.00,  1.00, 0.00, -0.836], [0.00,  0.00,  1.00,  0.670], [0.00,  0.00,  0.00,  1.00]])
-ikparam1 = IkParameterization(Tee1[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
+# ikparam1 = IkParameterization(Tee1[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
 ikparam2 = IkParameterization(Tee2[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
 ikparam3 = IkParameterization(Tee3[0:3,3],ikmodel2.iktype) # build up the translation3d ik query
 handle = manip.GetIkSolver()
-sol1 = manip.FindIKSolution(ikparam1,IkFilterOptions.CheckEnvCollisions)
+# sol1 = manip.FindIKSolution(ikparam1,IkFilterOptions.CheckEnvCollisions)
 sol2 = manip.FindIKSolution(ikparam2,IkFilterOptions.CheckEnvCollisions)
 sol3 = manip.FindIKSolution(ikparam3,IkFilterOptions.CheckEnvCollisions)
 # print "Ik solver:", success # Ik solver: [ 5.55111512e-16 -3.96993549e-12  1.57000000e+00  1.11022302e-16  0.00000000e+00  0.00000000e+00] with manip.GetIkParameterization(IkParameterization.Type.Translation3D)
-print "Ik solver:", sol1 # Ik solver: [ [-1.11022302e-16  8.99614938e-10  1.57000000e+00  1.11022302e-16  0.00000000e+00  0.00000000e+00]] with ikparam
-robot.SetDOFValues(sol1,ikmodel2.manip.GetArmIndices())
+# print "Ik solver:", sol1 # Ik solver: [ [-1.11022302e-16  8.99614938e-10  1.57000000e+00  1.11022302e-16  0.00000000e+00  0.00000000e+00]] with ikparam
+# robot.SetDOFValues(sol1,ikmodel2.manip.GetArmIndices())
 raw_input("cont2?")
 robot.SetDOFValues(sol2,ikmodel2.manip.GetArmIndices())
+print "Ik solver-2:", sol2
 raw_input("cont3?")
 robot.SetDOFValues(sol3,ikmodel2.manip.GetArmIndices())
+print "Ik solver-2:", sol2
 # basemanip.MoveToHandPosition([Tee2[0:3,3]],maxiter=1000,maxtries=1,seedik=4)
 # robot.WaitForController(0)
 raw_input("done")
 sys.exit()
-
-
-# print "testing..."
-# successrate, wrongrate = ikmodel2.testik(str(100))
-# print "iktest:", successrate, wrongrate
- 
-
-
 
     
 raw_input('Never here')
