af6e0b1331a7adaa8c5cdceed17b6d616671a6df
gizem <gizemates@iyte.edu.tr>
2021-03-24 00:05:37 +0100

    working ik_solver_node

--- src/ik_solver_node.py
+++ src/ik_solver_node.py
@@ -2,55 +2,18 @@
 # -*- coding: utf-8 -*-
 
 """
-This is the main node calculating the human pose.
+This is the main node calculating robot's joint angles based on given Pose().
 
 """
-# imports
 import rospy
-import time
-from Classes.IMU_subscriber_class_v2 import IMUsubscriber
-import Classes.body as body
-from sensor_msgs.msg import JointState
+from Classes.ik_solver_class import IKSolver
 
-joint_pos_kf = []
-joint_angles_kf = JointState()
-IMU = IMUsubscriber()
-index = 0
-
-
-def human_init():
-    joint_angles_kf.name = ['left_shoulder_2', 'left_shoulder_0', 'left_shoulder_1', 'left_elbow_2', 'left_elbow_0', 'left_elbow_1', 'left_wrist_2', 'left_wrist_0', 'left_wrist_1']
-    joint_angles_kf.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
-
-if __name__ == "__main__":
-    # Create initial readings and pose_init_shoulder
-    human_init()
-    IMU.init_subscribers_and_publishers()
-    body.initialize_pose(IMU)
-    start = time.time()
-    prev = 0
-    prev2 = 0
-    # rate = rospy.Rate(50)
-
-    while not rospy.is_shutdown():
-        now = time.time()
-        # print "++++", index
-        index += 1
-        # print "total: ", (now - start)
-        # print "interval:", (now - prev), "calibration_flag:", IMU.calibration_flag
-        if IMU.calibration_flag < 20:
-            IMU.update2(joint_angles_kf)
-            IMU.r.sleep()
-            prev = now
-            index = 0
-        else:
-            if (index == 5):
-                joint_pos_kf = body.update_pose(IMU)
-                joint_angles_kf.position = body.calculate_joint_angles(joint_pos_kf)
-                index = 0
-                # print "kf:", (now - prev2), "calibration_flag:", IMU.calibration_flag
-                prev2 = now
-            IMU.update2(joint_angles_kf)
-            IMU.r.sleep()
-            prev = now
+if __name__ == "__main__": 
+	iksolver = IKSolver(START_NODE=True, rate=100)
+	iksolver.init_subscribers_and_publishers()
+	start_time = rospy.Time.now()
+	while not rospy.is_shutdown():
+		current_time = rospy.Time.now()
+		print "Elapsed time iksolver:", (current_time-start_time)
+		iksolver.update()
+		iksolver.r.sleep()
