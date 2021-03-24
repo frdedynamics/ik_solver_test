aca5c83da839f0ab043408758d8fd0591bbe7508
gizem <gizemates@iyte.edu.tr>
2021-03-24 02:15:07 +0100

    things which should be in mapper, moved to MapperClass

--- src/Classes/ik_solver_class.py
+++ src/Classes/ik_solver_class.py
@@ -86,62 +86,59 @@ class IKSolver:
 		self.joint_states.name = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
 		self.joint_states.position = [0.0, 0.0, pi/2, 0.0, 0.0, 0.0]
 		self.ee_goal = Vector3()
-		self.wrist_pose = Pose(Point(0.422, 0.016, 0.513), Quaternion(-0.000, 1.000, -0.000, -0.000))  ## This shouldn't change actually. You can make it constant. # rosrun tf tf_echo /world /tool0
-			# - Translation: [0.422, 0.016, 0.513]
-			# - Rotation: in Quaternion [-0.000, 1.000, -0.000, -0.000]
-							# in RPY (radian) [3.142, -0.000, -3.141]
-							# in RPY (degree) [180.000, -0.004, -179.987]
-		self.tool_pose = Pose(Point(0.422, 0.192, 0.418), Quaternion(-0.000, 0.707, 0.707, -0.000))  ## rosrun tf tf_echo /world /tool0
-			# - Translation: [0.422, 0.192, 0.418]
-			# - Rotation: in Quaternion [-0.000, 0.707, 0.707, -0.000]
-            # in RPY (radian) [1.571, -0.000, -3.141]
-            # in RPY (degree) [90.000, -0.001, -179.989]
 		
-		self.r = rospy.Rate(rate)
 		if START_NODE == True:
 			rospy.init_node("ik_solver_node")
 			print "ik_solver_node initialized"
+
+
+	def start_node(self, rate):
+		'''
+		NOTE: only one node should be calling this function.
+		'''
+		rospy.init_node('ik_solver_node', anonymous=False)
+		print "ik_solver_node initialized"
+		self.r = rospy.Rate(rate)
 				
 			    
-    def init_subscribers_and_publishers(self):
-			self.pub = rospy.Publisher('/joint_states', JointState, queue_size=1)
-			self.sub_hand_pose = rospy.Subscriber('/hand_pose', Pose, self.sub_hand_pose)
-			self.sub_wrist_pose = rospy.Subscriber('/wrist_pose', Pose, self.sub_hand_pose)
-			# self.log_start_time = rospy.get_time()
-			_ROSTIME_START = rospy.get_time()
-			print "ik_solver_node pub/sub initialized"
+	def init_subscribers_and_publishers(self):
+		self.pub = rospy.Publisher('/joint_states', JointState, queue_size=1) # /ur5_joint_position_cmd
+		# self.sub_hand_pose = rospy.Subscriber('/hand_pose', Pose, self.sub_hand_pose)
+		# self.sub_wrist_pose = rospy.Subscriber('/wrist_pose', Pose, self.sub_hand_pose)
+		self.sub_Tee_pose = rospy.Subscriber('/Tee_pose', Pose, self.sub_Tee_pose)
+		# self.log_start_time = rospy.get_time()
+		_ROSTIME_START = rospy.get_time()
+		print "ik_solver_node pub/sub initialized"
 
 
-    def update(self):
-			self.calculate_joint_angles(self.Tee)
-			self.joint_states.header.stamp = rospy.Time.now()
-			self.pub.publish(self.joint_states)
+	def update(self):
+		self.calculate_joint_angles()
+		self.joint_states.header.stamp = rospy.Time.now()
+		self.pub.publish(self.joint_states)
 			
 
-		def calculate_joint_angles(self, Tee):
-			'''
-			Given ee_goal, calculate joint angles. Do I need to pull ee_goal?
-			@params ee_goal: type np.array(4x4) HTM
-			'''
-			if (type(Tee)==np.ndarray) and (Tee.shape == (4,4)):
-				# self.Tee = Tee
-				self.ikparam = IkParameterization(self.Tee[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
-				self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
-				self.robot.SetDOFValues(self.sol,self.ikmodel.manip.GetArmIndices())
-				self.joint_states.position = self.robot.GetDOFValues()
+	def calculate_joint_angles(self):
+		'''
+		Given ee_goal, calculate joint angles. Do I need to pull ee_goal?
+		@params ee_goal: type np.array(4x4) HTM
+		'''
+		if (type(self.Tee)==np.ndarray) and (self.Tee.shape == (4,4)):
+			# self.Tee = Tee
+			self.ikparam = IkParameterization(self.Tee[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
+			self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
+			self.robot.SetDOFValues(self.sol,self.ikmodel.manip.GetArmIndices())
+			self.joint_states.position = self.robot.GetDOFValues()
 
-			else:
-				print "Unknown ee_type"
+		else:
+			print "Unknown ee_type"
 
 
-    def sub_hand_pose(self, msg):
-			'''
-			Subscribes hand_pose, converts it to Tee
-			'''
-			hand_pose = msg
-			param_z = DHmatrices.ee_goal_calculate(hand_pose, self.wrist_pose)
-			Tee_pose = Pose(Point(hand_pose.position.x, hand_pose.position.y, param_z), hand_pose.orientation)
-			self.Tee = DHmatrices.pose_to_htm(Tee_pose)
+	def sub_Tee_pose(self, msg):
+		'''
+		Subscribes Tee_pose {Pose()}, converts it to Tee {np.array()}
+		'''
+		Tee_pose = msg
+		self.Tee = DHmatrices.pose_to_htm(Tee_pose)
 			
 		
 
