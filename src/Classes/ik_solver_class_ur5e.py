3b734b33ba7ff472f73a5471d23569f69c09e80f
gizem <gizemates@iyte.edu.tr>
2021-04-11 00:46:14 +0200

    catch no solution cases and skip

--- src/Classes/ik_solver_class_ur5e.py
+++ src/Classes/ik_solver_class_ur5e.py
@@ -14,7 +14,7 @@ from math import sqrt, pi
 import numpy as np
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Vector3
-from geometry_msgs.msg import Pose, Quaternion, Point
+from geometry_msgs.msg import Pose
 from std_msgs.msg import Int8
 
 from openravepy import *
@@ -41,7 +41,7 @@ class IKSolver:
 		self.env.Load(robot_path) # load a scene
 		self.env.SetViewer('qtcoin') # start the viewer
 		self.robot = self.env.GetRobots()[0] # get the first robot
-		print "Dof:", self.robot.GetDOFValues()
+		print "Dof", self.robot.GetDOFValues()
 
 		# Set IK model
 		if ikmodel==1:
@@ -50,14 +50,12 @@ class IKSolver:
 			self.iktype = IkParameterization.Type.Translation3D
 		else:
 			sys.exit("IK type not known")
-
+			
 		self.ikmodel = databases.inversekinematics.InverseKinematicsModel(robot=self.robot,iktype=self.iktype)
-		print "ikmodel file name:", self.ikmodel.getfilename(), self.ikmodel.load()
 		if not self.ikmodel.load():
 			print "New IK model is creating.."
 			self.ikmodel.autogenerate()
 			print "IK model created"
-		print "ikmodel file name:", self.ikmodel.getfilename(), self.ikmodel.load()
 		print "Load:", self.ikmodel.load()
 		print "Filename:", self.ikmodel.getfilename()
 		print "IKname:", self.ikmodel.getikname()
@@ -68,44 +66,21 @@ class IKSolver:
 		self.manip = self.robot.GetActiveManipulator()
 		self.Tee_current = self.manip.GetEndEffectorTransform() # get end effector
 
-		print "Tee_current", self.Tee_current
-		# dummy_input = raw_input()
-		"""
-		The transformation from wrist_3_link to tool 0 is fixed
-			- Translation: [0.000, 0.082, 0.000]
-			- Rotation: in Quaternion [-0.707, 0.000, 0.000, 0.707]	
-		"""
-		# fix_pose = Pose(Point(0.000, 0.000, 0.000), Quaternion(0.000, 0.000, 0.707, 0.707))
-		# fix_htm = DHmatrices.pose_to_htm(fix_pose)
-		# tee_corrected = np.matmul(np.asarray(self.Tee_current), fix_htm)
-		# print tee_corrected
-
-		m1 = np.array([[0.0, 1.0, 0.0, 0.817], [1.0, 0.0, 0.0, 0.0812], [0.0, 0.0, -1.0, 0.0629], [0.0, 0.0, 0.0, 1.0]])
-		m2 = np.array([[1.0, 0.0, 0.0, 0.817], [0.0, 0.0, -1.0, 0.234], [0.0, 1.0, 0.0, 0.0629], [0.0, 0.0, 0.0, 1.0]])
-
-		m3 = np.matmul(np.linalg.inv(m1), m2)
-		m3_pose = DHmatrices.htm_to_pose(m3)
-		print "m3:", m3_pose
-
-		sys.exit()
-
-		self.Twrist = self.robot.GetLinks()[2].GetTransform() # get wrist transform
-		self.Twrist_pose = DHmatrices.htm_to_pose(self.Twrist)
-
 		# Set joint limits
-		# self.robot.SetDOFValues([0.0,0.0,pi/2,0.0,0.0,0.0]) ## you may need to check this values.
+		self.robot.SetDOFValues([0.0,-1.57,1.57,0.0,0.0,0.0]) ## you may need to check this values.
 		lower = np.concatenate((np.array([-0.01, -(pi/2-0.01), pi/2-0.01]), np.array([1., 1., 1.])*-3.14159265))
 		upper = np.concatenate((np.array([0.01, -(pi/2-0.01), pi/2+0.01]), np.array([1., 1., 1.])*3.14159265))
-		# self.robot.SetDOFLimits(lower, upper) ## So annoying but skip now
-		print "DOF limits:", self.robot.GetDOFLimits()
+		# self.robot.SetDOFLimits(lower, upper)
+		# print "DOF limits:", self.robot.GetDOFLimits()
 
 		# EE poses
 		Tee1 = np.array([[0.00,  1.00,  0.00,  1.18], [1.00,  0.00,  0.00, -0.743], [-0.00,  0.00, -1.00,  1.011], [0.00,  0.00,  0.00,  1.00]])
 		Tee2 = np.array([[0.00,  0.00,  -1.00,  0.496], [ 1.00,  0.00,  0.00, -0.743], [0.00,  -1.00, 0.00,  0.555], [ 0.00,  0.00,  0.00,  1.00]])
-		Tee3 = np.array([[1.00,  0.00, 0.00,  -0.817], [0.00,  0.00, -1.00, -0.232], [0.00,  1.00,  0.00,  0.620], [0.00,  0.00,  0.00,  1.00]])
+		Tee3 = np.array([[1.00,  0.00, 0.00,  0.704], [0.00,  1.00, 0.00, -0.836], [0.00,  0.00,  1.00,  0.670], [0.00,  0.00,  0.00,  1.00]])
 		# self.Tee_current = np.array([1.00,  0.00, 0.00,  0.00], [0.00,  1.00, 0.00,  0.00], [0.00,  0.00, 1.00,  0.00], [0.00,  0.00, 0.00,  1.00])
-		self.Tee_current = Tee3 ## for test only - constantly read by self.manip.GetEndEffectorTransform() # get end effector
-		self.Tee_goal = np.zeros((4,4), dtype=np.float32) # gonna be calculated by ik
+		self.Tee_current = Tee1 ## for test only - constantly read by self.manip.GetEndEffectorTransform() # get end effector
+		# self.Tee_goal = np.zeros((4,4), dtype=np.float32) # gonna be calculated by ik
+		self.Tee_goal = Tee1
 
 
 		# IK parametrization init
@@ -119,14 +94,12 @@ class IKSolver:
 		self.joint_states = JointState()
 		## TODO: parametrize such that robot.GetJointNames()
 		self.joint_states.name = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
-		self.joint_states.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+		self.joint_states.position = [0.0, 0.0, pi/2, 0.0, 0.0, 0.0]
 		self.ee_goal = Vector3()
 		self.test_joints = JointState()
-		self.test_joints.position = [0,0.0,0,0.0,0.0,0.0]
-		# Tee: [0.8172 0.0812 0.0629 1.    ]
-		# joints: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
-
+		self.test_joints.position = [0.0,1.57,0.0,0.0,0.0,0.0]
+		
+		
 		if START_NODE == True:
 			rospy.init_node("ik_solver_node")
 			self.r = rospy.Rate(rate)
@@ -140,15 +113,15 @@ class IKSolver:
 		rospy.init_node('ik_solver_node', anonymous=False)
 		print "ik_solver_node initialized"
 		# self.r = rospy.Rate(rate)
-
-
+				
+			    
 	def init_subscribers_and_publishers(self):
-		self.pub = rospy.Publisher('/joint_states_openrave', JointState, queue_size=1)
-		self.pub_calculated_tee = rospy.Publisher('/Tee_calculated', Pose, queue_size=1)
-		self.pub_Twrist_pose = rospy.Publisher('/Twrist_pose', Pose, queue_size=1)
+		# self.pub = rospy.Publisher('/joint_states', JointState, queue_size=1) # /ur5_joint_position_cmd
+		self.pub_test = rospy.Publisher('/test_msg', Vector3, queue_size=1) # /ur5_joint_position_cmd
+		self.pub_calculated_tee = rospy.Publisher('/Tee_calculated', Pose, queue_size=1) # /ur5_joint_position_cmd
 		# self.sub_hand_pose = rospy.Subscriber('/hand_pose', Pose, self.sub_hand_pose)
 		# self.sub_wrist_pose = rospy.Subscriber('/wrist_pose', Pose, self.sub_hand_pose)
-		self.sub_Tee_pose = rospy.Subscriber('/Tee_mapper_goal_pose', Pose, self.sub_Tee_pose)
+		self.sub_Tee_pose = rospy.Subscriber('/Tee_goal_pose', Pose, self.sub_Tee_pose)
 		self.sub_test_joint = rospy.Subscriber('/test_joints', JointState, self.sub_test_joint)
 		self.sub_selector = rospy.Subscriber('/selector', Int8, self.sub_selector)
 		# self.log_start_time = rospy.get_time()
@@ -157,30 +130,21 @@ class IKSolver:
 
 
 	def update(self):
-		tee_goal = self.Tee_goal
+		# tee_goal = self.Tee_goal
 		# self.calculate_joint_angles2(tee_goal)
+		# self.Tee_goal = np.array([[0.00,  1.00,  0.00,  0.817], [1.00,  0.00,  0.00, -0.232], [0.00,  0.00, -1.00,  0.062], [0.00,  0.00,  0.00,  1.00]])
+		self.calculate_joint_angles()
 		# self.joint_states.header.stamp = rospy.Time.now()
 		# self.pub.publish(self.joint_states)
-		self.test_joints.header.stamp = rospy.Time.now()
-		self.pub.publish(self.test_joints)
-
 		self.Tee_current = self.manip.GetEndEffectorTransform()
 		self.Tee_goal_pose = DHmatrices.htm_to_pose(self.Tee_current)
+		self.pub_test.publish(test_pub_msg)
 		self.pub_calculated_tee.publish(self.Tee_goal_pose)
-
-
-		self.Twrist = self.robot.GetLinks()[5].GetTransform() # get wrist transform. The pivot point
-		self.Twrist_pose = DHmatrices.htm_to_pose(self.Twrist)
-		self.pub_Twrist_pose.publish(self.Twrist_pose)
-
-		## DEBUG purpose only
+		
 		# print "self.test_joints.position", self.test_joints.position
-		# self.robot.SetDOFValues(self.test_joints.position)
-		print "Tee:", self.Tee_current[:,3]
-		print "joints:", self.joint_states.position
-		# print "Twrist_pose:", self.Twrist_pose
-		# print "Tee_pose:", DHmatrices.htm_to_pose(self.Tee_current)
-
+		# self.robot.SetDOFValues(self.test_joints.position) 
+		# print "Tee:", self.Tee_current
+			
 
 	def calculate_joint_angles(self):
 		'''
@@ -188,17 +152,24 @@ class IKSolver:
 		@params ee_goal: type np.array(4x4) HTM
 		'''
 		if (type(self.Tee_goal)==np.ndarray) and (self.Tee_goal.shape == (4,4)):
-			# self.Tee = Tee
-			self.ikparam = IkParameterization(self.Tee_goal[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
-			self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
-			self.robot.SetDOFValues(self.sol,self.ikmodel.manip.GetArmIndices())
-			self.joint_states.position = self.robot.GetDOFValues()
 			print "Tee_goal:", self.Tee_goal
-			print "joint positions:", self.joint_states.position
+			try:
+				self.ikparam = IkParameterization(self.Tee_goal[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
+				self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
+				self.robot.SetDOFValues(self.sol,self.ikmodel.manip.GetArmIndices())
+				self.joint_states.position = self.robot.GetDOFValues()
+				if self.sol is not None and len(self.sol) > 0: # if found, then break
+					print len(self.sol)
+				else:
+					raise openrave_exception("No solution")
+			except openrave_exception, e:
+				print e
+			# print "Tee_goal:", self.Tee_goal
+			# print "joint positions:", self.joint_states.position
 
 		else:
 			print "Unknown ee_type"
-
+	
 	def calculate_joint_angles2(self, tee_goal):
 		'''
 		Given ee_goal, calculate joint angles. Do I need to pull ee_goal?
@@ -221,41 +192,33 @@ class IKSolver:
 		'''
 		Tee_goal_pose = msg
 		print "Tee_goal_pose:", Tee_goal_pose
-		self.Tee_goal = DHmatrices.pose_to_htm(Tee_goal_pose)
-
-
+		# self.Tee_goal = DHmatrices.pose_to_htm(Tee_goal_pose)
+		
+		
 	def sub_test_joint(self, msg):
 		'''
 		Subscribes Tee_pose {Pose()}, converts it to Tee {np.array()}
 		'''
 		self.test_joints.position = list(msg.position)
-
-
+		
 	def sub_selector(self, msg):
 		'''
-		This is only for test purpose
+		This is only for test purpose 
 		'''
+		
 		selector = msg.data
+		print "Selection detected", selector
 		if selector == 1:
-			self.Tee_goal = np.array([[0.0, 1.0, -0.01, 0.577], [1.0, 0.0, 0.0, -0.743], [0.0, -0.01, -1.0, 1.618], [0.0, 0.0, 0.0, 1.0]])
+			self.Tee_goal = np.array([[0.00,  1.00,  0.00,  0.817], [1.00,  0.00,  0.00, -0.232], [0.00,  0.00, -1.00,  0.062], [0.00,  0.00,  0.00,  1.00]])
 		elif selector == 2:
 			self.Tee_goal = np.array([[0.0, 1.0, -0.01, 0.462], [1.0, 0.0, 0.0, -0.743], [0.0, -0.01, -1.0, 1.734], [0.0, 0.0, 0.0, 1.0]])
+		elif selector == 3:
+			self.Tee_goal = np.array([[0.0, 1.0, -0.01, 0.4352], [1.0, 0.0, 0.0, -0.324243], [0.0, -0.01, -1.0, 2.234], [0.0, 0.0, 0.0, 1.0]])
 		else:
 			print "non registered selection"
+			
+			
+			
+		
 
 
-	def fix_base(self, tee):
-		'''
-		m3: position: 
-			x: 0.15280000000000002
-			y: 0.0
-			z: 0.0
-			orientation: 
-			x: 0.5
-			y: 0.5
-			z: -0.5
-			w: -0.5
-		'''
-		fix_pose = Pose(Point(0.153, 0.0, 0.0), Quaternion(0.5, 0.5, -0.5, -0.5))
-		fix_htm = DHmatrices.pose_to_htm(fix_pose)
-		return np.matmul(np.asarray(tee), fix_htm)
