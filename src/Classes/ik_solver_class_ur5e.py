88d91cba2c00f139f6faaf3d1194d13b6d1fa03e
gizem <gizemates@iyte.edu.tr>
2021-04-12 01:08:38 +0200

    all valid tee poses. Joint limits are still issue

--- src/Classes/ik_solver_class_ur5e.py
+++ src/Classes/ik_solver_class_ur5e.py
@@ -42,14 +42,18 @@ class IKSolver:
 		self.env.Load(robot_path) # load a scene
 		self.env.SetViewer('qtcoin') # start the viewer
 		viewer = self.env.GetViewer()
-		viewer.SetCamera([[0.,  0., -1., 2.],
-							[1.,  0.,  0., 0.],
+		# viewer.SetCamera([[0.,  0., -1., 2.],
+		# 					[1.,  0.,  0., 0.],
+		# 					[0., -1.,  0., .5],
+		# 					[0.,  0.,  0., 1.]])
+		viewer.SetCamera([[1.,  0., 0., 0.],
+							[0.,  0.,  1., -1.5],
 							[0., -1.,  0., .5],
 							[0.,  0.,  0., 1.]])
 		self.robot = self.env.GetRobots()[0] # get the first robot
 		print "Dof", self.robot.GetDOFValues()
-		RaveSetDebugLevel(DebugLevel.Debug)
-
+		# RaveSetDebugLevel(DebugLevel.Debug)
+				
 		# Set IK model
 		if ikmodel==1:
 			self.iktype = IkParameterization.Type.Transform6D
@@ -72,31 +76,81 @@ class IKSolver:
 		self.taskmanip = interfaces.TaskManipulation(self.robot)
 		self.manip = self.robot.GetActiveManipulator()
 		self.Tee_current = self.manip.GetEndEffectorTransform() # get end effector
+		print "selected ee:", self.manip.GetEndEffector()
 
 		# Set joint limits
-		self.robot.SetDOFValues([0.0,-1.57,1.57,0.0,0.0,0.0]) ## you may need to check this values.
+		null = [pi/2, -pi/2, 0.0, 0.0, 0.0, 0.0]
+		home = [pi/2, -pi/2, 0.0, pi, -pi/2, 0.0]
+		
+		
+		home2 = [pi/2, -pi/2., pi/2, pi, -pi/2, 0.0]
+		self.robot.SetDOFValues(home2) 
+		print "home:", self.manip.GetEndEffectorTransform() # get end effector
+		dummy_input = raw_input("Next?")
+		self.Twrist = self.robot.GetLink('wrist_1_link').GetTransform()
+		print self.Twrist  ## don't update this. Initial is our pivot point. Otherwise it changes with wrist_1 rotation
+		Tee_home = np.asarray(self.manip.GetEndEffectorTransform())
+		print "Tee_home:", Tee_home
+
+		test2 = [pi/2, -pi/2., pi/2, pi+pi/6, -pi/2, 0.0]
+		self.robot.SetDOFValues(test2)
+		print "test2:", self.manip.GetEndEffectorTransform() # get end effector
+		dummy_input = raw_input("Next?")
+		Tee2 = np.asarray(self.manip.GetEndEffectorTransform())
+		print "Tee2:", Tee2
+
+		test3 = [pi/2, -pi/2., pi/2, pi, -pi/2+pi/7, 0.0]
+		self.robot.SetDOFValues(test3)
+		print "test3:", self.manip.GetEndEffectorTransform() # get end effector
+		dummy_input = raw_input("Next?")
+		Tee3 = np.asarray(self.manip.GetEndEffectorTransform())
+		print "Tee3:", Tee3
+
 		lower = np.concatenate((np.array([-0.01, -(pi/2-0.01), pi/2-0.01]), np.array([1., 1., 1.])*-3.14159265))
 		upper = np.concatenate((np.array([0.01, -(pi/2-0.01), pi/2+0.01]), np.array([1., 1., 1.])*3.14159265))
-		# self.robot.SetDOFLimits(lower, upper)
+		self.robot.SetDOFLimits(lower, upper)
 		# print "DOF limits:", self.robot.GetDOFLimits()
 
-		# EE poses
-		Tee1 = np.array([[0.00,  1.00,  0.00,  1.18], [1.00,  0.00,  0.00, -0.743], [-0.00,  0.00, -1.00,  1.011], [0.00,  0.00,  0.00,  1.00]])
-		Tee2 = np.array([[0.00,  0.00,  -1.00,  0.496], [ 1.00,  0.00,  0.00, -0.743], [0.00,  -1.00, 0.00,  0.555], [ 0.00,  0.00,  0.00,  1.00]])
-		Tee3 = np.array([[1.00,  0.00, 0.00,  0.704], [0.00,  1.00, 0.00, -0.836], [0.00,  0.00,  1.00,  0.670], [0.00,  0.00,  0.00,  1.00]])
+		# # EE poses
+		# Tee1 = np.array([[0.00,  -1.00,  0.00,  -0.0812], [1.00,  0.00,  0.00, 0.3922], [-0.00,  0.00, 1.00,  0.687], [0.00,  0.00,  0.00,  1.00]])
+		# Tee2 = np.array([[0.00,  0.00,  -1.00,  0.496], [ 1.00,  0.00,  0.00, -0.743], [0.00,  -1.00, 0.00,  0.555], [ 0.00,  0.00,  0.00,  1.00]])
+		# Tee3 = np.array([[1.00,  0.00, 0.00,  0.704], [0.00,  1.00, 0.00, -0.836], [0.00,  0.00,  1.00,  0.670], [0.00,  0.00,  0.00,  1.00]])
 		# self.Tee_current = np.array([1.00,  0.00, 0.00,  0.00], [0.00,  1.00, 0.00,  0.00], [0.00,  0.00, 1.00,  0.00], [0.00,  0.00, 0.00,  1.00])
-		self.Tee_current = Tee1 ## for test only - constantly read by self.manip.GetEndEffectorTransform() # get end effector
+		# self.Tee_current = Tee1 ## for test only - constantly read by self.manip.GetEndEffectorTransform() # get end effector
 		# self.Tee_goal = np.zeros((4,4), dtype=np.float32) # gonna be calculated by ik
-		self.Tee_goal = Tee1
+		# self.Tee_goal = Tee1
 	
 
 
 		# IK parametrization init
-		self.ikparam = IkParameterization(self.Tee_current[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
+		# self.ikparam = IkParameterization(self.Tee_current[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
+		# self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
+		dummy_input = raw_input("Start Tee_home")
+		self.ikparam = IkParameterization(Tee_home[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
 		self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
+		self.robot.SetDOFValues(self.sol, self.ikmodel.manip.GetArmIndices())
+		dummy_input = raw_input("Next Tee2")
+
+		self.ikparam = IkParameterization(Tee2[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
+		self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
+		self.robot.SetDOFValues(self.sol, self.ikmodel.manip.GetArmIndices())
+		dummy_input = raw_input("Next Tee3")
+
+		self.ikparam = IkParameterization(Tee3[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
+		self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
+		self.robot.SetDOFValues(self.sol, self.ikmodel.manip.GetArmIndices())
+		dummy_input = raw_input("Done")
+		sys.exit()
+
 
 		# Init robot pose
 		self.robot.SetDOFValues(self.sol, self.ikmodel.manip.GetArmIndices())
+
+		Tee = self.manip.GetEndEffectorTransform()
+		Tee_pose = DHmatrices.htm_to_pose(Tee)
+		# print "Tee:", Tee
+		dummy_input = raw_input("Done?")
+		sys.exit()
 
 		# Updated parameters
 		self.joint_states = JointState()
@@ -124,14 +178,13 @@ class IKSolver:
 				
 			    
 	def init_subscribers_and_publishers(self):
-		# self.pub = rospy.Publisher('/joint_states', JointState, queue_size=1) # /ur5_joint_position_cmd
-		self.pub_test = rospy.Publisher('/test_msg', Vector3, queue_size=1) # /ur5_joint_position_cmd
-		self.pub_calculated_tee = rospy.Publisher('/Tee_calculated', Pose, queue_size=1) # /ur5_joint_position_cmd
-		# self.sub_hand_pose = rospy.Subscriber('/hand_pose', Pose, self.sub_hand_pose)
-		# self.sub_wrist_pose = rospy.Subscriber('/wrist_pose', Pose, self.sub_hand_pose)
-		self.sub_Tee_pose = rospy.Subscriber('/Tee_goal_pose', Pose, self.sub_Tee_pose)
+		self.pub = rospy.Publisher('/joint_states_openrave', JointState, queue_size=1) # calculated joints for ur5
+		self.pub_calculated_tee = rospy.Publisher('/Tee_calculated', Pose, queue_size=1) # For debug purposes
+		# self.pub_test = rospy.Publisher('/test_msg', Vector3, queue_size=1) #
+
+		self.sub_Tee_pose = rospy.Subscriber('//Tee_mapper_goal_pose', Pose, self.sub_Tee_pose)
 		self.sub_test_joint = rospy.Subscriber('/test_joints', JointState, self.sub_test_joint)
-		self.sub_selector = rospy.Subscriber('/selector', Int8, self.sub_selector)
+		# self.sub_selector = rospy.Subscriber('/selector', Int8, self.sub_selector)
 		# self.log_start_time = rospy.get_time()
 		_ROSTIME_START = rospy.get_time()
 		print "ik_solver_node pub/sub initialized"
@@ -140,15 +193,17 @@ class IKSolver:
 	def update(self):
 		# tee_goal = self.Tee_goal
 		# self.calculate_joint_angles2(tee_goal)
+
+		# CALCULATE JOINT ANGLES AND PUBLISH BASED ON WHAT \human_robot_mapper SENT
 		# self.Tee_goal = np.array([[0.00,  1.00,  0.00,  0.817], [1.00,  0.00,  0.00, -0.232], [0.00,  0.00, -1.00,  0.062], [0.00,  0.00,  0.00,  1.00]])
-		
 		self.calculate_joint_angles()
-		# self.joint_states.header.stamp = rospy.Time.now()
-		# self.pub.publish(self.joint_states)
+		self.joint_states.header.stamp = rospy.Time.now()
+		self.pub.publish(self.joint_states)
+
+		# CALCULATE TEE POSE IN OPENRV -- DEBUD PURPOSE
 		self.Tee_current = self.manip.GetEndEffectorTransform()
-		self.Tee_goal_pose = DHmatrices.htm_to_pose(self.Tee_current)
-		self.pub_test.publish(test_pub_msg)
-		self.pub_calculated_tee.publish(self.Tee_goal_pose)
+		self.Tee_current_pose = DHmatrices.htm_to_pose(self.Tee_current)
+		self.pub_calculated_tee.publish(self.Tee_current_pose)
 		
 		# print "self.test_joints.position", self.test_joints.position
 		# self.robot.SetDOFValues(self.test_joints.position) 
@@ -161,24 +216,26 @@ class IKSolver:
 		@params ee_goal: type np.array(4x4) HTM
 		'''
 		global Tee_fail
-		comparison = self.Tee_goal == Tee_fail
 		if (type(self.Tee_goal)==np.ndarray) and (self.Tee_goal.shape == (4,4)):
+			comparison = self.Tee_goal == Tee_fail
 			if not comparison.all():
 				print "Tee_goal:", self.Tee_goal
 				try:
 					self.ikparam = IkParameterization(self.Tee_goal[0:3,3], self.ikmodel.iktype) # build up the translation3d ik query
-					self.sol = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
-					self.robot.SetDOFValues(self.sol,self.ikmodel.manip.GetArmIndices())
+					self.sols = self.manip.FindIKSolution(self.ikparam, IkFilterOptions.CheckEnvCollisions)
+					self.robot.SetDOFValues(self.sols,self.ikmodel.manip.GetArmIndices())
 					self.joint_states.position = self.robot.GetDOFValues()
-					if self.sol is not None and len(self.sol) > 0: # if found, then break
-						print len(self.sol)
+					if self.sols is not None and len(self.sols) > 0: # if found, then break
+						print len(self.sols)
+						for sol in self.sols:
+							print sol
 					else:
 						Tee_fail = self.Tee_goal
 						raise openrave_exception("No solution")
 				except openrave_exception, e:
 					print e
 				# print "Tee_goal:", self.Tee_goal
-				# print "joint positions:", self.joint_states.position
+				print "joint positions:", self.joint_states.position
 			else:
 				print "The same invalid Tee"
 
@@ -208,7 +265,7 @@ class IKSolver:
 		'''
 		Tee_goal_pose = msg
 		print "Tee_goal_pose:", Tee_goal_pose
-		# self.Tee_goal = DHmatrices.pose_to_htm(Tee_goal_pose)
+		self.Tee_goal = DHmatrices.pose_to_htm(Tee_goal_pose)
 		
 		
 	def sub_test_joint(self, msg):
